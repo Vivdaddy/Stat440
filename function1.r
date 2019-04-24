0e8dfdbe36a95c4738a6b7bf33f9b47881b82902
Ryan <ryanvoyack@gmail.com>
2019-04-24 13:55:59 -0400

    commit

--- function1.r
+++ function1.r
@@ -1,15 +1,19 @@
 
+'%!in%' <- function(x,y)!('%in%'(x,y))
+
 find_complete_subgraphs <- function(data, min.max = c(451,452)){
   # data <- graph_from_data_frame(data, directed = F)
   
   graph <- list(data)
   complete_subgraphs <- list(list())
   vertices_csg <- list(list())
-  
+  leftout <- list(c())
+    
   iter <<- 1
   # this while loop is defined for some sort of stopping criteria
-  # while(length(unique(c(graph[[iter]][,1], graph[[iter]][,2]))) > 2000){
+  
   while(min.max[1] > 2){
+  # while(length(unique(c(graph[[iter]][,1], graph[[iter]][,2]))) > 2000){
   # while(min.max[2] > 100){
     complete_subgraphs[[iter]] <- list()
     while(length(complete_subgraphs[[iter]]) == 0){
@@ -20,36 +24,33 @@ find_complete_subgraphs <- function(data, min.max = c(451,452)){
     # complete_subgraphs updates before the other two lists
     # over any one iteration, the length of the graph list is ahead of other two lists by 1
     if(length(complete_subgraphs[[iter]]) > 1){
-      
       warning("special case")
       graph[[(iter+1)]] <- list()
       vertices_csg[[iter]] <- list()
-      for(i in 1: length(complete_subgraphs[[iter]])){
+      leftout[[iter]] <- list()
+      for(i in 1:length(complete_subgraphs[[iter]])){
         max.ind <- i
-        # get vertices for maximal clique  #tryCatch(
+        # get vertices for maximal clique     #tryCatch(
         vert <- as.numeric(vertices(complete_subgraphs[[iter]][[max.ind]])[[1]]) # ,error=function(e) stop("COCK SUCK"))
-        vertices_csg[[iter]][[i]] <- unique(c(graph[[iter]][,1], graph[[iter]][,2]))[vert] #, error=function(e) return(list(graph, complete_subgraphs, vertices_csg)))
+        vertices_csg[[iter]][[i]] <- unique(c(graph[[iter]][,1], graph[[iter]][,2]))[vert] #, error=function(e) return(list(graph[-1], complete_subgraphs, vertices_csg)))
         
-        temp <- graph[[iter]][unique(c(which(graph[[iter]][,1] %in% vertices_csg[[iter]][[i]]), which(graph[[iter]][,2] %in% vertices_csg[[iter]][[i]]))),]
-        temp1 <- unique(c(temp[,1], temp[,2]))
-        
-        # We want an edgelist with all of these nodes:
+        # we want the subsetted dataframes in this special case to subset eachother, so as to follow the main iterative algorithm in order to coincide with the topological properties of the graphs that we wish to utilize
         temp2 <- c()
         for(j in 1:i){
-          # tryCatch(temp2 <- unique(c(temp2, vertices_csg[[iter]][[j]])), error=function(e) return(list(graph, complete_subgraphs, vertices_csg)))
-          temp2 <- unique(c(temp2, vertices_csg[[iter]][[j]]))
+          # tryCatch(temp2 <- unique(c(temp2, vertices_csg[[iter]][[j]])), error=function(e) return(list(graph[-1], complete_subgraphs, vertices_csg)))
+          temp2 <- c(temp2, vertices_csg[[iter]][[j]])
         }
         temp2 <- unique(temp2)
-        # tryCatch(graph[[(iter+1)]][[i]] <- graph[[iter]][-which(graph[[iter]][,1] %in%  temp2), ], error=function(e) return(list(graph[[iter]][1:(i-1)], complete_subgraphs, vertices_csg)))   # vertices_csg[[iter]][[i]]), ]
-        graph[[(iter+1)]][[i]] <- graph[[iter]][-which(graph[[iter]][,1] %in%  temp2), ]
-        # tryCatch(graph[[(iter+1)]][[i]] <- graph[[(iter+1)]][[i]][-which(graph[[(iter+1)]][[i]][,2] %in% temp2), ], error=function(e)return(list(graph[[iter]][1:(i-1)], complete_subgraphs, vertices_csg))) # vertices_csg[[iter]][[i]]), ]
-        graph[[(iter+1)]][[i]] <- graph[[(iter+1)]][[i]][-which(graph[[(iter+1)]][[i]][,2] %in% temp2), ]
         
-        warning(length(unique(c(graph[[(iter)]][,1], graph[[(iter)]][,2]))) - length(unique(c(graph[[(iter+1)]][[i]][,1], graph[[(iter+1)]][[i]][,2]))) - length(vertices_csg[[iter]][[i]]), " (iter: ", i, ")")
-        # ^ ?! i think i did this warning wrong
+        # tryCatch(graph[[(iter+1)]][[i]] <- graph[[iter]][-which(graph[[iter]][,1] %in% temp2 | graph[[iter]][,2] %in% temp2), ], error=function(e) return(list(graph[-1], complete_subgraphs, vertices_csg)))
+        graph[[(iter+1)]][[i]] <- graph[[iter]][-which(graph[[iter]][,1] %in% temp2 | graph[[iter]][,2] %in% temp2), ]
+
+        temp3 <- unique(c(graph[[(iter)]][,1], graph[[(iter)]][,2])) # this is all of the nodes in the graph that just got subsetted
+        temp3 <- temp3[which(temp3 %!in% unique(c(graph[[(iter+1)]][[i]][,1], graph[[(iter+1)]][[i]][,2])))] # this is all of the nodes that we left behind: the vertices and some first neighbors who didnt neighbor anyone else
+        leftout[[iter]] <- temp3[which(temp3 %!in% vertices_csg[[iter]][[i]])] #these are those neighbors who were left behind
       }
-      # we want to leave the function at this point
-      return(list(graph, complete_subgraphs, vertices_csg))
+      # we want to leave the function at this point:
+      return(list(graph[-1], complete_subgraphs, vertices_csg, leftout))
     }
     
     max.ind <- 1
@@ -61,37 +62,40 @@ find_complete_subgraphs <- function(data, min.max = c(451,452)){
     vert <- as.numeric(vertices(complete_subgraphs[[iter]][[max.ind]])[[1]]) # get vertices for maximal clique
     vertices_csg[[iter]] <- unique(c(graph[[iter]][,1], graph[[iter]][,2]))[vert]
         
-    temp <- graph[[iter]][unique(c(which(graph[[iter]][,1] %in% vertices_csg[[iter]]), which(graph[[iter]][,2] %in% vertices_csg[[iter]]))),]
-    # ^ This includes the CSG and its neighbors, the csg has perimeter (say) with length = 451 nodes.
-    # length(unique(c(temp[,1], temp[,2]))) # there are 7198-388 nodes outside of it that they neighbor.
-    temp1 <- unique(c(temp[,1], temp[,2]))
-    
     # We want an edgelist (with edges) for all of these vertices:
-    graph[[(iter+1)]] <- graph[[iter]][-which(graph[[iter]][,1] %in% vertices_csg[[iter]]), ]
-    graph[[(iter+1)]] <- graph[[(iter+1)]][-which(graph[[(iter+1)]][,2] %in% vertices_csg[[iter]]), ]
-
-    warning(length(unique(c(graph[[(iter)]][,1], graph[[(iter)]][,2]))) - length(unique(c(graph[[(iter+1)]][,1], graph[[(iter+1)]][,2]))) - length(vertices_csg[[iter]]))
-    # if(length(unique(c(graph[[(iter)]][,1], graph[[(iter)]][,2]))) - length(unique(c(graph[[(iter+1)]][,1], graph[[(iter+1)]][,2]))) - length(vertices_csg[[iter]]) != 0){
-    #   leftout[[iter]] <- unique(c(graph[[(iter)]][,1], graph[[(iter)]][,2])) %in% unique(c(graph[[(iter+1)]][,1], graph[[(iter+1)]][,2]))
-    # }else{
-    #   leftout[[iter]] <- 0
-    # }
-
+    graph[[(iter+1)]] <- graph[[iter]][-which(graph[[iter]][,1] %in% vertices_csg[[iter]] | graph[[iter]][,2] %in% vertices_csg[[iter]]), ]  # graph[[(iter+1)]] <- graph[[(iter+1)]][-which(graph[[(iter+1)]][,2] %in% vertices_csg[[iter]]), ]
+    
+    temp1 <- unique(c(graph[[(iter)]][,1], graph[[(iter)]][,2])) # this is all of the nodes in the graph that just got subsetted
+    temp1 <- temp1[which(temp1 %!in% unique(c(graph[[(iter+1)]][,1], graph[[(iter+1)]][,2])))] # this is all of the nodes that we left behind: the vertices and some first neighbors who didnt neighbor anyone else
+    leftout[[iter]] <- temp1[which(temp1 %!in% vertices_csg[[iter]])] #these are those neighbors who were left behind
+    # warning(length(unique(c(graph[[(iter)]][,1], graph[[(iter)]][,2]))) - length(unique(c(graph[[(iter+1)]][,1], graph[[(iter+1)]][,2]))) - length(vertices_csg[[iter]]))
     
     iter <<- iter + 1
-    if(length(unique(c(graph[[iter]][,1], graph[[iter]][,2]))) == (nrow(graph[[iter]])*2)){
-      # stopping criteria
-      break
-    }
+    # if(length(unique(c(graph[[iter]][,1], graph[[iter]][,2]))) == (nrow(graph[[iter]])*2)){
+    #   # stopping criteria
+    #   break
+    # }
     warning("iter")
   }
-  return(list(graph, complete_subgraphs, vertices_csg))
+  return(list(graph[-1], complete_subgraphs, vertices_csg, leftout))
 }
+
 
 plz <- find_complete_subgraphs(data2)
 warnings()
-yay2 <- find_complete_subgraphs(plz[[1]][[6]][[1]], min.max = c(185,186))
 
+nrow(plz[[1]][[5]][[1]])
+nrow(plz[[1]][[5]][[2]])
+length(unique(c(plz[[1]][[5]][[2]][,1], plz[[1]][[5]][[2]][,2])))
+length(unique(c(plz[[1]][[5]][[1]][,1], plz[[1]][[5]][[1]][,2])))
+length(unique(c(plz[[1]][[5]][[2]][,1], plz[[1]][[5]][[2]][,2], plz[[1]][[5]][[1]][,1], plz[[1]][[5]][[1]][,2])))
+# ^ we see that its subsetted, as it should have done in the instance of a "special case"
+
+while(T){
+  plz
+}
+
+yay2 <- find_complete_subgraphs(plz[[1]][[5]][[1]], min.max = c(185,186))
 
 length(yay[[3]][[5]])
 
